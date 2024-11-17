c55bc54d6ced723dd4df15aa3969cde2e43f05e2
Nur Blessed <blessednur67@gmail.com>
2024-11-17 16:22:04 +0100

    dashboard z-indexed fixed

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -352,7 +352,7 @@ function Page() {
             <SignOut />{" "}
           </div>
 
-          <div className="flex-1 -z-1 flex flex-col overflow-hidden">
+          <div className="flex-1 z-10 flex flex-col overflow-hidden">
             <main className="flex-1 overflow-auto p-4 md:p-6">
               <div className="max-w-[1270px] mx-auto">
                 {activeTab === "dashboard" && <DashboardContent />}
